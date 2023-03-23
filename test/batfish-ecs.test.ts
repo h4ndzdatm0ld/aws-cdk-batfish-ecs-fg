f1e237199c9840191f1c9781887e222a64e6dd45
h4ndzdatm0ld <hugotinoco@icloud.com>
2023-03-23 04:33:46 +0000

    Initial commit

---
+++ test/batfish-ecs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BatfishEcs from '../lib/batfish-ecs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/batfish-ecs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BatfishEcs.BatfishEcsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
